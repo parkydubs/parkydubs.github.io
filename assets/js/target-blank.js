388c5c9bcebdb7be265a1961683cd1511da8d15c
parkydubs <parkerawang@gmail.com>
2025-08-01 22:35:48 -0700

    overwriting custom sass/css

--- assets/js/target-blank.js
+++ assets/js/target-blank.js
@@ -1,8 +1,16 @@
 // open link in new window, unless the link is in the top nav bar or to a project
-const a = document.getElementsByTagName('a');
-for (const l of a) {
-    if (!l.offsetParent.className.match(/^greedy-nav|archive__item$/) 
-        || l.innerHTML == "Resume") { // exception of resume in nav bar
-        l.setAttribute('target', '_blank');
+// const a = document.getElementsByTagName('a');
+// for (const l of a) {
+//     if (!l.offsetParent.className.match(/^greedy-nav|archive__item$/) 
+//         || l.innerHTML == "Resume") { // exception of resume in nav bar
+//         l.setAttribute('target', '_blank');
+//     }
+// }
+
+
+for (const l of document.querySelectorAll('a')) {
+    const inNavOrProject = l.closest('.greedy-nav, .archive__item');
+    if (!inNavOrProject || l.textContent.trim() === 'Resume') {
+      l.setAttribute('target', '_blank');
     }
-}
+  }
--- index.md
+++ index.md
@@ -8,7 +8,7 @@ show_title: false
 
 # Hello, I'm Parker!
 
-I am a [Boston College][bc] student studying Computer Science with minors in Accounting for Finance and Consulting, and Music. In my free time, you can find me swimming (barely staying afloat), honing my skills with my violin, and cheffing-up delicious home-cooked meals in the kitchen!
+I am a student studying Computer Science with minors in Accounting for Finance & Consulting and Music at [Boston College][bc]. In my free time, you can find me swimming (barely staying afloat), honing my skills with my violin, and cheffing up delicious home-cooked meals in the kitchen!
 
 
 <!-- add intro/bio here -->
